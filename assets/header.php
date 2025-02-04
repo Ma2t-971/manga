43a6b3367cf7cfabd414abf487f838bd187f7fab
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-02-04 14:07:29 +0100

    Header update

--- assets/header.php
+++ assets/header.php
@@ -2,11 +2,9 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <script src="./script.js" defer></script>
-    <title>Document</title>
+    <title>Mangas</title>
 </head>
 <body>
--- formAddManga.php
+++ formAddManga.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <title>Mangas</title>
-</head>
-<body>
+<?php require_once('./assets/header.php');?>
     <header>
         <nav>
             <a href="./index.php">Accueil</a>
