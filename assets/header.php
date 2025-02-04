325d49978eccde593e20de7569ce0e15f95f154d
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-02-04 16:33:30 +0100

    Fix

--- assets/header.php
+++ assets/header.php
@@ -5,7 +5,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="shortcut icon" href="./img/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="../assets/img/favicon.png" type="image/png">
     <title>Mangas</title>
 </head>
 <body>
